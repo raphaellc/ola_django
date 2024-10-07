a2231f5d82d564874869a90e577ce3dc4258b581
RAPHAEL LEITE CAMPOS <raphaellc@users.noreply.github.com>
2024-10-07 19:52:06 -0300

    atualização do nome do form

--- ola_django/primeiro_app/forms.py
+++ ola_django/primeiro_app/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from .models import Pessoa
 
-class PessoaForm(forms.ModelForm):
+class PessoaCreateForm(forms.ModelForm):
     class Meta:
         model = Pessoa
         fields = ['nome', 'idade', 'email'] # alternativa para usar todos os campos -> '__all__'
--- ola_django/primeiro_app/views.py
+++ ola_django/primeiro_app/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, ListView, UpdateView, DetailView
 from .models import Pessoa
-from .forms import PessoaForm, PessoaUpdateForm 
+from .forms import PessoaCreateForm, PessoaUpdateForm 
 # Create your views here.
 #criação da tela de cadastro de pessoa
 class PessoaCreateView(CreateView):
     model = Pessoa
-    form_class = PessoaForm
+    form_class = PessoaCreateForm
     template_name = 'cadastrar_pessoa.html'
     success_url = reverse_lazy('lista_pessoas')
 
